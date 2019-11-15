796fa2416ff5e841a38098aa8240005176332dbe
riekure <s2darl0uvgia24@gmail.com>
2019-11-15 15:24:11 +0900

    第28章の例を追加

--- TestDrivenDevTest/CalcTest.cs
+++ TestDrivenDevTest/CalcTest.cs
@@ -16,5 +16,22 @@ namespace TestDrivenDevTest
         {
             return augend + addend;
         }
+
+        [Test]
+        public void TestSumArray()
+        {
+            Assert.AreEqual(5, Sum(new int[] {5}));
+            Assert.AreEqual(12, Sum(new int[]{5, 7}));
+        }
+
+        private int Sum(int[] values)
+        {
+            var sum = 0;
+            foreach (var v in values)
+            {
+                sum = sum + v;
+            }
+            return sum;
+        }
     }
 }
